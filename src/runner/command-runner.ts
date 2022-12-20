d4e0702e67bc37733bf329edc0ea456aad0e8b23
casianaoprut <nicoleta-casiana.oprut@endava.com>
2022-12-20 23:25:23 +0200

    code refactoring and runAll flow

--- src/runner/command-runner.ts
+++ src/runner/command-runner.ts
@@ -1,37 +1,39 @@
 import {exec} from 'child_process'
-import {CommandContext} from '../model/Command'
+import {Command, CommandContext, instanceOfCommand} from '../model/Command'
 import {osType} from '@dxworks/cli-common'
 
 export function runCommand(commandContext: CommandContext): void {
 
-    if (commandContext.command.universalCommand) {
-        executeCommand(commandContext.command.universalCommand)
-    }
-    switch (osType) {
-        case 'windows':
-            if (commandContext.command.windows)
-                executeCommand(commandContext.command.windows)
-            break
-        case 'linux':
-            if (commandContext.command.linux) {
-                executeCommand(commandContext.command.linux)
+    if (!instanceOfCommand(commandContext.command)) {
+        executeCommand(<string>commandContext.command)
+    } else {
+        const command = <Command>commandContext.command
+        switch (osType) {
+            case 'windows':
+                if (command.windows)
+                    executeCommand(command.windows)
                 break
-            }
-            if (commandContext.command.unix)
-                executeCommand(commandContext.command.unix)
-            break
-        case 'mac':
-            if (commandContext.command.mac) {
-                executeCommand(commandContext.command.mac)
+            case 'linux':
+                if (command.linux) {
+                    executeCommand(command.linux)
+                    break
+                }
+                if (command.unix)
+                    executeCommand(command.unix)
                 break
-            }
-            if (commandContext.command.unix)
-                executeCommand(commandContext.command.unix)
-            break
+            case 'mac':
+                if (command.mac) {
+                    executeCommand(command.mac)
+                    break
+                }
+                if (command.unix)
+                    executeCommand(command.unix)
+                break
+        }
     }
 }
 
-function executeCommand(command:string): void {
+function executeCommand(command: string): void {
     exec(command, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`)
--- src/runner/mission-runner.ts
+++ src/runner/mission-runner.ts
@@ -1,14 +1,24 @@
-import {MissionContext} from '../model/MissionContext'
-import {runInstrument} from './instrument-runner'
+import {Instrument} from '../model/Instrument'
+import {runCommand} from './command-runner'
+import {missionContext} from '../context/mission-context'
 
-export function runMission(missionContext: MissionContext): void {
-    missionContext.mission.instruments.forEach((missionInstrument) => {
-        const actualInstrument = missionContext.instruments.find((instrument) => instrument.name == missionInstrument.id)
-        if (actualInstrument) {
-            runInstrument({
-                instrument: actualInstrument,
-                actions: missionInstrument.actions,
-            })
-        }
+export function runMission(): void {
+    const instruments = missionContext.getInstruments()
+    if (missionContext.runAll)
+        instruments.forEach((instrument) => {
+            runInstrument(instrument)
+        })
+    else {
+        missionContext.getRunnableInstruments().forEach((runnableInstrument) => {
+            const instrument = instruments.find((instrument) => instrument.id == runnableInstrument)
+            if (instrument)
+                runInstrument(instrument)
+        })
+    }
+}
+
+function runInstrument(instrument: Instrument): void {
+    instrument.actions.forEach((action) => {
+        action.commandsContext.forEach((commandContext) => runCommand(commandContext))
     })
 }
